76b3b1f4f2ccfeb8b063aca0c704e3defd860d43
lydiahallie <lydiajuliettehallie@gmail.com>
2017-09-08 14:14:39 -0400

    Add data in file, add styling

--- src/assets/data/methods.js
+++ src/assets/data/methods.js
@@ -1,25 +1,103 @@
 const METHODS = [
   {
     id: 1,
-    name: ".charAt()",
-    description: "Returns the character at the specified index (position)",
-    goal: "find the index",
-    dataType: "string"
+    "name": ".charAt()",
+    "description": "Returns the character at the specified index (position)",
+    "goal": "Find",
+    "dataType": "string",
+    "image": "https://image.ibb.co/mF8pBv/Screen_Shot_2017_09_07_at_20_41_28.png",
+    "URL": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charAt"
   },
   {
     id: 2,
-    name: ".concat()",
-    description: "Joins two or more strings, and returns a new joined strings",
-    goal: "join strings",
-    dataType: "string, array"
+    "name": ".concat()",
+    "description": "Joins two or more strings, and returns a new joined strings",
+    "goal": "join",
+    "dataType": "array",
+    "image": "https://image.ibb.co/gKoajF/Screen_Shot_2017_09_08_at_12_55_09.png",
+    "URL": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat?v=b"
   },
   {
     id: 3,
-    name: ".every()",
-    description: "Checks if every element in an array pass a test",
-    goal: "check items",
-    dataType: "array"
+    "name": ".every()",
+    "description": "checks if every element in an array pass a test",
+    "goal": "check",
+    "dataType": "array",
+    "image": "https://image.ibb.co/jsk0jF/Screen_Shot_2017_09_07_at_20_33_44.png",
+    "URL": "https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Global_Objects/Array/every"
   },
+  {
+    id: 4,
+    "name": ".fill()",
+    "description": "Fill the elements in an array with a static value.",
+    "goal": "change",
+    "dataType": "array",
+    "image": "https://image.ibb.co/khzPda/Screen_Shot_2017_09_07_at_20_37_18.png",
+    "URL": "https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Global_Objects/Array/fill"
+  },
+  {
+    id: 5,
+    "name": ".filter()",
+    "description": "Creates a new array with every element in an array that pass a test.",
+    "goal": "find",
+    "dataType": "array",
+    "image": "https://image.ibb.co/dpaA1v/Screen_Shot_2017_09_08_at_11_04_42.png",
+    "URL": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter"
+  },
+  {
+    id: 6,
+    "name": ".find()",
+    "description": "Returns the value of the first element in an array that pass a test.",
+    "goal": "find",
+    "dataType": "array",
+    "image": "https://image.ibb.co/grK3Mv/Screen_Shot_2017_09_08_at_11_07_55.png",
+    "URL": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find"
+  },
+  {
+    id: 7,
+    "name": ".findIndex()",
+    "description": "Returns the index of the first element in an array that pass a test.",
+    "goal": "find",
+    "dataType": "array",
+    "image":"https://image.ibb.co/hDs9Ta/Screen_Shot_2017_09_08_at_11_22_03.png",
+    "URL":"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex"
+  },
+  {
+    id: 8,
+    "name": ".forEach()",
+    "description": "Calls a function for each array element.",
+    "goal": "find",
+    "dataType": "array",
+    "image": "https://image.ibb.co/ngoYoa/Screen_Shot_2017_09_08_at_11_27_24.png",
+    "URL": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach"
+  },
+  {
+    id: 9,
+    "name": ".indexOf()",
+    "description": "Search the array for an element and returns its position.",
+    "goal": "find",
+    "dataType": "array",
+    "image": "https://image.ibb.co/ifQ2gv/Screen_Shot_2017_09_08_at_11_46_54.png",
+    "URL": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf"
+  },
+  {
+    id: 10,
+    "name": ".isArray()",
+    "description": "checks whether an object is an array.",
+    "goal": "check",
+    "dataType": "other",
+    "image":"https://image.ibb.co/ekOEda/Screen_Shot_2017_09_08_at_12_03_08.png",
+    "URL": "https://developer.mozilla.org/nl/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray"
+  },
+  {
+    id: 11,
+    "name": ".join()",
+    "description": "joins all elements of an array into a string.",
+    "goal": "join",
+    "dataType": "array",
+    "image": "https://image.ibb.co/fH0Kda/Screen_Shot_2017_09_08_at_13_59_38.png",
+    "URL": "https://www.w3schools.com/jsref/jsref_join.asp"
+  }
 ];
 
 export default METHODS;
--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,20 +1,136 @@
 import React, { Component } from 'react';
 import map from 'lodash/map';
+import filter from 'lodash/filter';
 import METHODS from '../../assets/data/methods.js';
+import {Row, Input} from 'react-materialize';
+import './styles.css';
 
 export default class Results extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      array: false,
+      string: false,
+      join: false,
+      find: false,
+      check: false,
+      change: false
+    };
+  }
+
+  filterItems = (event) => {
+    // event.target.value will be either 'small', 'medium', or 'large'
+    // Toggle this.state.small, medium, and large to either true or false.
+    const toggledValue = !this.state[event.target.value];
+    this.setState({ [event.target.value]: toggledValue });
+  }
+
   render() {
-    return(
-      <div className="results">
-        {map(METHODS, (method)=> (
-          <div className="result">
-            <div className="method-details">
-              <h1 id="product-name">{method.name}</h1>
-              <h4 id="product-description">{method.description}</h4>
+    const { array, string, join, find, check, change } = this.state;
+    let methods = METHODS;
+
+    // Filter products if filters have been selected by user.
+    const hasSelectedFilter = array || string || check || find || join || change;
+    if (hasSelectedFilter) {
+      if (array) {
+        methods = filter(methods, ["dataType", "array"])
+      }
+      if (string) {
+        methods = filter(methods, ["dataType", "string"])
+      }
+      if (join) {
+        methods = filter(methods, ["goal", "join"])
+      }
+      if (check) {
+        methods = filter(methods, ["goal", "check"])
+      }
+      if (find) {
+        methods = filter(methods, ["goal", "find"])
+      }
+      if (change) {
+        methods = filter(methods, ["goal", "change"])
+      }
+    }
+
+    return (
+      <div>
+        <Row>
+          <div className="picker-wrapper">
+            <div className="row">
+                <div className="picker">
+                  <p>Pick Your Datatype</p>
+                  <Input
+                    onClick={this.filterItems}
+                    type="checkbox"
+                    id="checkbox"
+                    value="string"
+                    disabled={array}
+                    className='filled-in'
+                    defaultChecked='checked'
+                  />
+                  <label htmlFor="category_dresses">String</label>
+
+                  <input
+                    onClick={this.filterItems}
+                    type="checkbox"
+                    id="checkbox"
+                    value="array"
+                    disabled={string}
+                  />
+                  <label htmlFor="category_tops">Array</label>
+                </div>
+              </div>
+
+              <div className="picker">
+                <p>Pick Your Goal</p>
+                <input
+                  onClick={this.filterItems}
+                  type="checkbox"
+                  id="checkbox"
+                  value="Join"
+                  disabled={check || find || change}
+                />
+                <label htmlFor="goal_join">Join</label>
+                <input
+                  onClick={this.filterItems}
+                  type="checkbox"
+                  id="checkbox"
+                  value="check"
+                  disabled={join || find || change}
+                />
+                <label htmlFor="goal_check">Check</label>
+                <input
+                  onClick={this.filterItems}
+                  type="checkbox"
+                  id="checkbox"
+                  value="find"
+                  disabled={join || check || change}
+                />
+                <label htmlFor="goal_check">Find</label>
+                <input
+                  onClick={this.filterItems}
+                  type="checkbox"
+                  id="checkbox"
+                  value="change"
+                  disabled={join || find || check}
+                />
+                <label htmlFor="goal_check">Change</label>
+              </div>
             </div>
-          </div>
-        ))}
+          </Row>
+          <div className="results">
+            {map(methods, (method)=> (
+              <div className="result">
+                <div className="method-details">
+                  <h1 id="method-name">{method.name}</h1>
+                  <h4 id="method-description">{method.description}</h4>
+                  <img src={method.image} />
+                  <p><a href={method.URL}>Documentation</a></p>
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
-    )
+   );
   }
 }
